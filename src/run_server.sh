67ee91b57a241fe088e103b78bf74bf33d25e915
frzzn38 <pablo.frezzno@gmail.com>
2022-09-04 00:52:39 +0200

    setup all needed env vars for server

--- src/run_server.sh
+++ src/run_server.sh
@@ -65,23 +65,8 @@ function start_server() {
 function apply_postinstall_config() {
     printf "\n### Applying Post Install Configuration...\n"
 
-    # Set the Autosave Interval
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "SaveWorldEveryMinutes" "$AUTOSAVE_INTERVAL"
-
-    # Set the Server game Port
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "SteamPort1" "$GAME_PORT"
-
-    # Set the Max Players
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "MaxPlayers" "$MAX_PLAYERS"
-
-    # Set the Mod names
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "Mods" "$MOD_NAMES"
-
-    # Set the Map names
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "Map" "$MAP_NAMES"
-
-    # Set the Mod Workshop IDs
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "WorkshopItems" "$MOD_WORKSHOP_IDS"
+    #Set the PVP
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "PVP" "$PVP"
 
     # Set the Pause on Empty Server
     "$EDIT_CONFIG" "$SERVER_CONFIG" "PauseEmpty" "$PAUSE_ON_EMPTY"
@@ -92,17 +77,71 @@ function apply_postinstall_config() {
     # Set the Server query Port
     "$EDIT_CONFIG" "$SERVER_CONFIG" "DefaultPort" "$QUERY_PORT"
 
-    # Set the Server RCON Password
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "RCONPassword" "$RCON_PASSWORD"
+    # Set the Mod names
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "Mods" "$MOD_NAMES"
 
-    # Set the Server RCON Port
-    "$EDIT_CONFIG" "$SERVER_CONFIG" "RCONPort" "$RCON_PORT"
+    # Set the Map names
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "Map" "$MAP_NAMES"
 
     # Set the Server Name
     "$EDIT_CONFIG" "$SERVER_CONFIG" "PublicName" "$SERVER_NAME"
 
+    # Set the Max Players
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "MaxPlayers" "$MAX_PLAYERS"
+
+    #Set the Minutes Per Page
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "MinutesPerPage" "$MINUTES_PER_PAGE"
+
+    # Set the Autosave Interval
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SaveWorldEveryMinutes" "$AUTOSAVE_INTERVAL"
+
+    #Set the Player Safehouse
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "PlayerSafehouse" "$PLAYER_SAFEHOUSE"
+
+    #Set the Safehouse Allow Trepass
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SafehouseAllowTrepass" "$SAFEHOUSE_ALLOW_TREPASS"
+
+    #Set the Safehouse Allow Loot
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SafehouseAllowLoot" "$SAFEHOUSE_ALLOW_LOOT"
+
+    #Set the Safehouse Allow Respawn
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SafehouseAllowRespawn" "$SAFEHOUSE_ALLOW_RESPAWN"
+
+    #Set the Safehouse Removal Time
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SafeHouseRemovalTime" "$SAFEHOUSE_REMOVAL_TIME"
+
+    #Set the Safehouse Allow Non Residential
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SafehouseAllowNonResidential" "$SAFEHOUSE_ALLOW_NON_RESIDENTIAL"
+
+    # Set the Server RCON Port
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "RCONPort" "$RCON_PORT"
+
+    # Set the Server RCON Password
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "RCONPassword" "$RCON_PASSWORD"
+
+    #Set the Discord Enable
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "DiscordEnable" "$DISCORD_ENABLE"
+
+    #Set the Discord Token
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "DiscordToken" "$DISCORD_TOKEN"
+
+    #Set the Discord Token
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "DiscordChannelID" "$DISCORD_CHANNEL_ID"
+
     # Set the Server Password
     "$EDIT_CONFIG" "$SERVER_CONFIG" "Password" "$SERVER_PASSWORD"
+
+    # Set the Server game Port
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "SteamPort1" "$GAME_PORT"
+
+    # Set the Mod Workshop IDs
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "WorkshopItems" "$MOD_WORKSHOP_IDS"
+
+    # Set the Mod Workshop IDs
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "WorkshopItems" "$MOD_WORKSHOP_IDS"
+
+    #Set the UPnP
+    "$EDIT_CONFIG" "$SERVER_CONFIG" "UPnP" "$UPNP"
 
     # Set the maximum amount of RAM for the JVM
     sed -i "s/-Xmx.*/-Xmx$MAX_RAM\",/g" "$SERVER_VM_CONFIG"
@@ -161,9 +200,6 @@ function set_variables() {
     # Set the Server Admin Password variable
     ADMIN_PASSWORD=${ADMIN_PASSWORD:-"changeme"}
 
-    # Set the Autosave Interval variable
-    AUTOSAVE_INTERVAL=${AUTOSAVE_INTERVAL:-"15"}
-
     # Set the IP address variable
     # NOTE: Project Zomboid cannot handle the IN_ANY address
     if [[ -z "$BIND_IP" ]] || [[ "$BIND_IP" == "0.0.0.0" ]]; then
@@ -174,42 +210,86 @@ function set_variables() {
     fi
     echo "$BIND_IP" > "$CONFIG_DIR/ip.txt"
 
-    # Set the IP Game Port variable
-    GAME_PORT=${GAME_PORT:-"8766"}
-
     # Set the game version variable
     GAME_VERSION=${GAME_VERSION:-"public"}
-
-    # Set the Max Players variable
-    MAX_PLAYERS=${MAX_PLAYERS:-"16"}
 
     # Set the Maximum RAM variable
     MAX_RAM=${MAX_RAM:-"4096m"}
 
-    # Set the Mods to use from workshop
-    MOD_NAMES=${MOD_NAMES:-""}
-    MOD_WORKSHOP_IDS=${MOD_WORKSHOP_IDS:-""}
-
-    # Set the Maps to use
-    MAP_NAMES=${MAP_NAMES:-"Muldraugh, KY"}
-
-    # Set the Pause on Empty variable
-    PAUSE_ON_EMPTY=${PAUSE_ON_EMPTY:-"true"}
-
-    # Set the Server Publicity variable
-    PUBLIC_SERVER=${PUBLIC_SERVER:-"true"}
-
-    # Set the IP Query Port variable
-    QUERY_PORT=${QUERY_PORT:-"16261"}
-
-    # Set the Server name variable
-    SERVER_NAME=${SERVER_NAME:-"ZomboidServer"}
-
-    # Set the Server Password variable
-    SERVER_PASSWORD=${SERVER_PASSWORD:-""}
-
     # Set Steam VAC Protection variable
     STEAM_VAC=${STEAM_VAC:-"true"}
+
+    #Set the PVP
+    PVP=${PVP:-"true"}
+
+    # Set the Pause on Empty Server
+    PAUSE_ON_EMPTY=${PAUSE_ON_EMPTY:-"true"}
+
+    # Set the Server Publicity status
+    PUBLIC_SERVER=${PUBLIC_SERVER:-"true"}
+
+    # Set the Server query Port
+    QUERY_PORT=${QUERY_PORT:-"16261"}
+
+    # Set the Mod names
+    MOD_NAMES=${MOD_NAMES:-""}
+
+    # Set the Map names
+    MAP_NAMES=${MAP_NAMES:-"Muldraugh, KY"}
+
+    # Set the Server Name
+    SERVER_NAME=${SERVER_NAME:-"ZomboidServer"}
+
+    # Set the Max Players
+    MAX_PLAYERS=${MAX_PLAYERS:-"16"}
+
+    #Set the Minutes Per Page
+    MINUTES_PER_PAGE=${MINUTES_PER_PAGE:-"1.0"}
+
+    # Set the Autosave Interval
+    AUTOSAVE_INTERVAL=${AUTOSAVE_INTERVAL:-"15"}
+
+    #Set the Player Safehouse
+    PLAYER_SAFEHOUSE=${PLAYER_SAFEHOUSE:-"false"}
+
+    #Set the Safehouse Allow Trepass
+    SAFEHOUSE_ALLOW_TREPASS=${SAFEHOUSE_ALLOW_TREPASS:-"true"}
+
+    #Set the Safehouse Allow Loot
+    SAFEHOUSE_ALLOW_LOOT=${SAFEHOUSE_ALLOW_LOOT:-"true"}
+
+    #Set the Safehouse Allow Respawn
+    SAFEHOUSE_ALLOW_RESPAWN=${SAFEHOUSE_ALLOW_RESPAWN:-"false"}
+
+    #Set the Safehouse Removal Time
+    SAFEHOUSE_REMOVAL_TIME=${SAFEHOUSE_REMOVAL_TIME:-"144"}
+
+    #Set the Safehouse Allow Non Residential
+    SAFEHOUSE_ALLOW_NON_RESIDENTIAL=${SAFEHOUSE_ALLOW_NON_RESIDENTIAL:-"false"}
+
+    #Set the Discord Enable
+    DISCORD_ENABLE=${DISCORD_ENABLE:-"false"}
+
+    #Set the Discord Token
+    DISCORD_TOKEN=${DISCORD_TOKEN:-""}
+
+    #Set the Discord Token
+    DISCORD_CHANNEL_ID=${DISCORD_CHANNEL_ID:-""}
+
+    # Set the Server Password
+    SERVER_PASSWORD=${SERVER_PASSWORD:-""}
+
+    # Set the Server game Port
+    GAME_PORT=${GAME_PORT:-"8766"}
+
+    # Set the Mod Workshop IDs
+    MOD_WORKSHOP_IDS=${MOD_WORKSHOP_IDS:-""}
+
+    #Set the UPnP
+    UPNP=${UPNP:-"true"}
+
+    #Set the Map Remote Player Visibility
+    MAP_REMOTE_PLAYER_VISIBILITY=${MAP_REMOTE_PLAYER_VISIBILITY:-"1"}
 
     # Set server type variable
     if [[ -z "$USE_STEAM" ]] || [[ "$USE_STEAM" == "true" ]]; then
